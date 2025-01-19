04a7f5dcac7e8485e2fb2b775b6277a4fc5a388b
Whitney Nsegbe <whitneynsegbe@gmail.com>
2025-01-19 00:01:14 -0500

    fixing db?

--- root/public/firebase/firebase.js
+++ root/public/firebase/firebase.js
@@ -1,11 +1,10 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase.js
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Import Firebase functions
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+// Firebase configuration object
 const firebaseConfig = {
     apiKey: "AIzaSyBK-0w354ZjMZVq0XH-jaGUzV0fwdHYUOg",
     authDomain: "ufoundit-78c43.firebaseapp.com",
@@ -18,4 +17,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Initialize Firebase Authentication
+const auth = getAuth(app);
+
+// Export auth to use it in other files
+export { auth, signInWithEmailAndPassword };
--- root/public/login-form.js
+++ root/public/login-form.js
@@ -1,3 +1,4 @@
+
 const loginForm = document.getElementById("login-form");
 
 loginForm.addEventListener("submit", async (e) => {
